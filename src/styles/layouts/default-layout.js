bd8621f3783753feb5cf04f92215901d5252a6bf
therese.rossi <therese.rossi@mail.com>
2020-05-03 02:06:36 +0200

    Add global style, theme and default layout.

---
+++ src/styles/layouts/default-layout.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../../styles/theme';
+import { GlobalStyle } from '../../styles/global';
+
+export const DefaultLayout = ({ children }) => (
+  <ThemeProvider theme={theme}>
+    <>
+      {children}
+      <GlobalStyle />
+    </>
+  </ThemeProvider>
+);
